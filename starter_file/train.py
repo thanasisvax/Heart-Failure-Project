14bdf03d8fd64d9d23ad2450d9b5f51d29dca5bb
thanasisvax <47087744+thanasisvax@users.noreply.github.com>
2020-11-11 19:00:08 +0000

    Update train.py

--- starter_file/train.py
+++ starter_file/train.py
@@ -5,51 +5,30 @@ import numpy as np
 from sklearn.metrics import mean_squared_error
 import joblib
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import OneHotEncoder
 import pandas as pd
 from azureml.core.run import Run
 from azureml.core.dataset import Dataset
 from azureml.data.dataset_factory import TabularDatasetFactory
 
-# TODO: Create TabularDataset using TabularDatasetFactory
-# Data is located at:
+# TabularDataset is located at:
 
-path1 = "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
-
+path1 = ""
+# Load Dataset into ds
 ds = TabularDatasetFactory.from_delimited_files(path = path1)
 
+#Split Target and Features in y and x respectively in the clean_data function
 
 def clean_data(data):
-    # Dict for cleaning data
-    months = {"jan":1, "feb":2, "mar":3, "apr":4, "may":5, "jun":6, "jul":7, "aug":8, "sep":9, "oct":10, "nov":11, "dec":12}
-    weekdays = {"mon":1, "tue":2, "wed":3, "thu":4, "fri":5, "sat":6, "sun":7}
 
     # Clean and one hot encode data
-    x_df = data.to_pandas_dataframe().dropna()
-    jobs = pd.get_dummies(x_df.job, prefix="job")
-    x_df.drop("job", inplace=True, axis=1)
-    x_df = x_df.join(jobs)
-    x_df["marital"] = x_df.marital.apply(lambda s: 1 if s == "married" else 0)
-    x_df["default"] = x_df.default.apply(lambda s: 1 if s == "yes" else 0)
-    x_df["housing"] = x_df.housing.apply(lambda s: 1 if s == "yes" else 0)
-    x_df["loan"] = x_df.loan.apply(lambda s: 1 if s == "yes" else 0)
-    contact = pd.get_dummies(x_df.contact, prefix="contact")
-    x_df.drop("contact", inplace=True, axis=1)
-    x_df = x_df.join(contact)
-    education = pd.get_dummies(x_df.education, prefix="education")
-    x_df.drop("education", inplace=True, axis=1)
-    x_df = x_df.join(education)
-    x_df["month"] = x_df.month.map(months)
-    x_df["day_of_week"] = x_df.day_of_week.map(weekdays)
-    x_df["poutcome"] = x_df.poutcome.apply(lambda s: 1 if s == "success" else 0)
-
-    y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
-    return x_df ,y_df
-
-
-# TODO: Split data into train and test sets.
+    x_df = data.to_pandas_dataframe()
+    y = x_df['DEATH_EVENT']
+    x = x_df.drop(['DEATH_EVENT'], axis = 1)
+    return x ,y
 
 x, y = clean_data(ds)
+
+# Split data into train and test sets.
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.20, random_state = 42)
 
