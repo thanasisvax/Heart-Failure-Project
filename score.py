660ece6055db3a661a7d3dacbaa43eee13a2ee05
thanasisvax <47087744+thanasisvax@users.noreply.github.com>
2020-11-15 16:19:17 +0000

    Add files via upload

--- score.py
+++ score.py
@@ -1,27 +1,25 @@
 import joblib
+import json
 import numpy as np
 import os
 from azureml.core.model import Model
-#from inference_schema.schema_decorators import input_schema, output_schema
-#from inference_schema.parameter_types.numpy_parameter_type import NumpyParameterType
-
+import time
 
 # The init() method is called once, when the web service starts up.
 #
 # Typically you would deserialize the model file, as shown here using joblib,
 # and store it in a global variable so your run() method can access it later.
 def init():
-    global model
+    global auto_model
+    #Print statement for appinsights custom traces:
+    print ("model initialized" + time.strftime("%H:%M:%S"))
 
     # The AZUREML_MODEL_DIR environment variable indicates
     # a directory containing the model file you registered.
-    #model_filename = 'Automl_run.model_id'
-    #model_path = os.path.join(os.environ['AZUREML_MODEL_DIR'], model_filename)
-    model_path = Model.get_model_path('model_name')
+    #model_filename = 'Automl_run.model_id' - Replace model_name with the AutoML ID.
+    model_path = Model.get_model_path('AutoMLc543f296e15')
 
     automl_model = joblib.load(model_path)   
-   # model = joblib.load(model_path)
-
 
 # The run() method is called each time a request is made to the scoring API.
 #
@@ -30,13 +28,20 @@ def init():
 # run() method parses and validates the incoming payload against
 # the example input you provide here. This will also generate a Swagger
 # API document for your web service.
-#@input_schema('data', NumpyParameterType(np.array([[0.1, 1.2, 2.3, 3.4, 4.5, 5.6, 6.7, 7.8, 8.9, 9.0]])))
-#@output_schema(NumpyParameterType(np.array([4429.929236457418])))
+
 def run(data):
     try:
-        result = model.predict(data)
+        data = np.array(json.loads(data)['data'])
+        result = auto_model.predict(data)
+        # Log the input and output data to appinsights:
+        info = {
+            "input": data,
+            "output": result.tolist()
+            }
+        print(json.dumps(info))
         # You can return any data type, as long as it is JSON serializable.
         return result.tolist()
     except Exception as e:
         error = str(e)
+        print (error + time.strftime("%H:%M:%S"))
         return error
